b32258cda256bbbce815649f572b0057f7af29cc
Patte <p.peltokorpi96@gmail.com>
2025-11-28 21:18:39 +0200

    Moved player registering logic to a separate component

---
+++ src/components/players/RegisterPlayers.vue
@@ -0,0 +1,25 @@
+<template>
+  <AddPlayer/>
+
+  <div id="playerlist" style="padding: 10px 0">
+    <span v-for="player in allPlayers" :key="player.id" style="width: 100%">
+      <PlayerRow :player="player"/>
+    </span>
+  </div>
+
+  <div style="text-align: center">
+    <StartGame/>
+  </div>
+</template>
+
+<script setup>
+  import AddPlayer from 'components/players/AddPlayer.vue';
+  import PlayerRow from 'components/players/PlayerRow.vue';
+  import { usePlayersStore } from "stores/players.js";
+  import { storeToRefs } from 'pinia';
+  import StartGame from "components/game/StartGame.vue";
+
+  const playersStore = usePlayersStore();
+
+  const { allPlayers } = storeToRefs(playersStore);
+</script>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,17 +1,7 @@
 <template>
   <q-page class="flex flex-center">
-    <div v-if="!gameIsOngoing">
-      <AddPlayer/>
-
-      <div id="playerlist" style="padding: 10px 0">
-        <span v-for="player in allPlayers" :key="player.id" style="width: 100%">
-          <PlayerRow :player="player"/>
-        </span>
-      </div>
-
-      <div style="text-align: center">
-        <StartGame/>
-      </div>
+    <div v-if="!gameIsOngoing" style="width: fit-content; height: fit-content;">
+      <RegisterPlayers/>
     </div>
     <div v-else>
       <!-- TODO TÄHÄN PELILOGIIKKAA -->
@@ -20,21 +10,12 @@
 </template>
 
 <script setup>
-  import AddPlayer from 'components/players/AddPlayer.vue';
-  import PlayerRow from 'components/players/PlayerRow.vue';
-  import { usePlayersStore } from "stores/players.js";
+  import RegisterPlayers from "components/players/RegisterPlayers.vue";
   import { useGameStore } from "stores/game.js";
   import { storeToRefs } from 'pinia';
-  import StartGame from "components/game/StartGame.vue";
-
-  const playersStore = usePlayersStore();
   const gameStore = useGameStore();
 
-  const { allPlayers } = storeToRefs(playersStore);
   const { gameIsOngoing } = storeToRefs(gameStore);
-
-
-
 </script>
 
 <style scoped>
