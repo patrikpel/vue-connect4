a62ddda45cdfb22ac3211e9b746b8409667ec470
Patte <p.peltokorpi96@gmail.com>
2025-11-28 22:43:36 +0200

    Adjusted game board layout

--- src/components/game/GameMatrix.vue
+++ src/components/game/GameMatrix.vue
@@ -1,27 +1,57 @@
 <template>
-  <div v-for="rowIndex in board" :key="'board-row-'+rowIndex" class="board-row">
-    <div v-for="(disc, colIndex) in rowIndex" :key="'board-col'+colIndex" class="board-col"
-         style="width: 20px; height: 20px; border: 1px solid black; background-color: red;" @click="insertDisc(rowIndex, colIndex)">
-        {{disc.player}}
-    </div>
+  <div class="board-grid"
+       :style="{
+         gridTemplateRows: `repeat(${board.length}, 1fr)`,
+         gridTemplateColumns: `repeat(${board[0].length}, 1fr)`,
+         width: '300px',
+         height: '300px',
+       }">
+
+    <template v-for="(row, rowIndex) in board" :key="'row-' + rowIndex">
+      <div v-for="(disc, colIndex) in row"
+           :key="'cell-' + rowIndex + '-' + colIndex"
+           class="board-cell"
+           :style="{
+             gridRow: rowIndex + 1,
+             gridColumn: colIndex + 1
+           }"
+           @click="insertDisc(rowIndex, colIndex)">
+        {{ disc.player }}
+      </div>
+    </template>
+
   </div>
 </template>
 
 <script setup>
-  import { useGameStore } from "stores/game.js";
-  import { storeToRefs } from 'pinia';
+import { useGameStore } from "stores/game.js";
+import { storeToRefs } from "pinia";
 
-  const gameStore = useGameStore();
-  const { board } = storeToRefs(gameStore);
+const gameStore = useGameStore();
+const { board } = storeToRefs(gameStore);
+
+function insertDisc(row, col) {
+  gameStore.insertDisc(row, col);
+}
 </script>
 
 <style scoped>
-  .board-col {
-    display: flex;
-    flex-direction: column;
-  }
-  .board-row {
-    display: flex;
-    flex-direction: row;
-  }
+.board-grid {
+  display: grid;
+  gap: 2px; /* optional spacing */
+  width: max-content;
+}
+
+.board-cell {
+  width: 30px;
+  height: 30px;
+  border: 1px solid black;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 12px;
+  border-radius: 100%;
+}
+
+
 </style>
